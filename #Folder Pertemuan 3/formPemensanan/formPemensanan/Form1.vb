eea027b4d30ef566e214b033d751bc9b5c5074bb
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2025-02-18 17:50:27 +0700

    formPemesanan

---
+++ #Folder Pertemuan 3/formPemensanan/formPemensanan/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+End Class
