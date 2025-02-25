0fe8155e79cc6e572ac71f71f5f7e34de767cdf0
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2025-02-25 11:00:44 +0700

    imageViewer

---
+++ #Folder Pertemuan 4/image viewer/imageViewer/imageViewer/Form1.Designer.vb
@@ -0,0 +1,379 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        ImageList1 = New ImageList(components)
+        StatusStrip1 = New StatusStrip()
+        ToolStripStatusReady = New ToolStripStatusLabel()
+        ToolStripStatusDimension = New ToolStripStatusLabel()
+        ToolStripStatusBits = New ToolStripStatusLabel()
+        ToolStripStatusResolution = New ToolStripStatusLabel()
+        ToolStripStatusRawFormat = New ToolStripStatusLabel()
+        ToolTip1 = New ToolTip(components)
+        tvDrive = New TreeView()
+        lvImage = New ListView()
+        chName = New ColumnHeader()
+        chExtension = New ColumnHeader()
+        chFileSize = New ColumnHeader()
+        chLastModified = New ColumnHeader()
+        chLastAccessed = New ColumnHeader()
+        chCreatedDate = New ColumnHeader()
+        chAttributes = New ColumnHeader()
+        PictureBox1 = New PictureBox()
+        SplitContainerUtama = New SplitContainer()
+        SplitContainerBawah = New SplitContainer()
+        SplitContainerBawah2 = New SplitContainer()
+        tcProperties = New TabControl()
+        TabPage1 = New TabPage()
+        PropertyGrid1 = New PropertyGrid()
+        TabPage2 = New TabPage()
+        lvExif = New ListView()
+        chProperties = New ColumnHeader()
+        chValue = New ColumnHeader()
+        StatusStrip1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(SplitContainerUtama, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainerUtama.Panel1.SuspendLayout()
+        SplitContainerUtama.Panel2.SuspendLayout()
+        SplitContainerUtama.SuspendLayout()
+        CType(SplitContainerBawah, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainerBawah.Panel1.SuspendLayout()
+        SplitContainerBawah.Panel2.SuspendLayout()
+        SplitContainerBawah.SuspendLayout()
+        CType(SplitContainerBawah2, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainerBawah2.Panel1.SuspendLayout()
+        SplitContainerBawah2.Panel2.SuspendLayout()
+        SplitContainerBawah2.SuspendLayout()
+        tcProperties.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' ImageList1
+        ' 
+        ImageList1.ColorDepth = ColorDepth.Depth32Bit
+        ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), ImageListStreamer)
+        ImageList1.TransparentColor = Color.Transparent
+        ImageList1.Images.SetKeyName(0, "9056941_drive_icon.png")
+        ImageList1.Images.SetKeyName(1, "299060_folder_icon.png")
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusReady, ToolStripStatusDimension, ToolStripStatusBits, ToolStripStatusResolution, ToolStripStatusRawFormat})
+        StatusStrip1.Location = New Point(0, 426)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(800, 24)
+        StatusStrip1.TabIndex = 0
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusReady
+        ' 
+        ToolStripStatusReady.Name = "ToolStripStatusReady"
+        ToolStripStatusReady.Size = New Size(42, 19)
+        ToolStripStatusReady.Text = "Ready."
+        ' 
+        ' ToolStripStatusDimension
+        ' 
+        ToolStripStatusDimension.BorderSides = ToolStripStatusLabelBorderSides.Left
+        ToolStripStatusDimension.BorderStyle = Border3DStyle.Etched
+        ToolStripStatusDimension.Name = "ToolStripStatusDimension"
+        ToolStripStatusDimension.Size = New Size(73, 19)
+        ToolStripStatusDimension.Text = "Dimensions"
+        ' 
+        ' ToolStripStatusBits
+        ' 
+        ToolStripStatusBits.BorderSides = ToolStripStatusLabelBorderSides.Left
+        ToolStripStatusBits.BorderStyle = Border3DStyle.Etched
+        ToolStripStatusBits.Name = "ToolStripStatusBits"
+        ToolStripStatusBits.Size = New Size(54, 19)
+        ToolStripStatusBits.Text = "# of bits"
+        ' 
+        ' ToolStripStatusResolution
+        ' 
+        ToolStripStatusResolution.BorderSides = ToolStripStatusLabelBorderSides.Left
+        ToolStripStatusResolution.BorderStyle = Border3DStyle.Etched
+        ToolStripStatusResolution.Name = "ToolStripStatusResolution"
+        ToolStripStatusResolution.Size = New Size(67, 19)
+        ToolStripStatusResolution.Text = "Resolution"
+        ' 
+        ' ToolStripStatusRawFormat
+        ' 
+        ToolStripStatusRawFormat.BorderSides = ToolStripStatusLabelBorderSides.Left
+        ToolStripStatusRawFormat.BorderStyle = Border3DStyle.Etched
+        ToolStripStatusRawFormat.Name = "ToolStripStatusRawFormat"
+        ToolStripStatusRawFormat.Size = New Size(74, 19)
+        ToolStripStatusRawFormat.Text = "Raw Format"
+        ' 
+        ' ToolTip1
+        ' 
+        ToolTip1.ToolTipIcon = ToolTipIcon.Info
+        ToolTip1.ToolTipTitle = "ImageViewer Tip:"
+        ' 
+        ' tvDrive
+        ' 
+        tvDrive.Dock = DockStyle.Fill
+        tvDrive.ImageIndex = 0
+        tvDrive.ImageList = ImageList1
+        tvDrive.Location = New Point(0, 0)
+        tvDrive.Name = "tvDrive"
+        tvDrive.SelectedImageIndex = 0
+        tvDrive.Size = New Size(190, 426)
+        tvDrive.TabIndex = 0
+        ToolTip1.SetToolTip(tvDrive, "Please select drive/folder that contain")
+        ' 
+        ' lvImage
+        ' 
+        lvImage.Columns.AddRange(New ColumnHeader() {chName, chExtension, chFileSize, chLastModified, chLastAccessed, chCreatedDate, chAttributes})
+        lvImage.GridLines = True
+        lvImage.Location = New Point(2, 0)
+        lvImage.Name = "lvImage"
+        lvImage.Size = New Size(604, 272)
+        lvImage.SmallImageList = ImageList1
+        lvImage.TabIndex = 0
+        ToolTip1.SetToolTip(lvImage, "Image file(s)")
+        lvImage.UseCompatibleStateImageBehavior = False
+        lvImage.View = View.Details
+        ' 
+        ' chName
+        ' 
+        chName.Text = "Name"
+        chName.Width = 200
+        ' 
+        ' chExtension
+        ' 
+        chExtension.Text = "Extension"
+        chExtension.Width = 80
+        ' 
+        ' chFileSize
+        ' 
+        chFileSize.Text = "File Size"
+        chFileSize.Width = 80
+        ' 
+        ' chLastModified
+        ' 
+        chLastModified.Text = "Last Modified"
+        chLastModified.Width = 100
+        ' 
+        ' chLastAccessed
+        ' 
+        chLastAccessed.Text = "Last Accessed"
+        chLastAccessed.Width = 100
+        ' 
+        ' chCreatedDate
+        ' 
+        chCreatedDate.Text = "Created Date"
+        chCreatedDate.Width = 100
+        ' 
+        ' chAttributes
+        ' 
+        chAttributes.Text = "Attributes"
+        chAttributes.Width = 90
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Dock = DockStyle.Fill
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(400, 148)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ToolTip1.SetToolTip(PictureBox1, "Double click to view this image in auto")
+        ' 
+        ' SplitContainerUtama
+        ' 
+        SplitContainerUtama.Dock = DockStyle.Fill
+        SplitContainerUtama.Location = New Point(0, 0)
+        SplitContainerUtama.Name = "SplitContainerUtama"
+        ' 
+        ' SplitContainerUtama.Panel1
+        ' 
+        SplitContainerUtama.Panel1.Controls.Add(tvDrive)
+        ' 
+        ' SplitContainerUtama.Panel2
+        ' 
+        SplitContainerUtama.Panel2.Controls.Add(SplitContainerBawah)
+        SplitContainerUtama.Size = New Size(800, 426)
+        SplitContainerUtama.SplitterDistance = 190
+        SplitContainerUtama.TabIndex = 1
+        ' 
+        ' SplitContainerBawah
+        ' 
+        SplitContainerBawah.Dock = DockStyle.Fill
+        SplitContainerBawah.Location = New Point(0, 0)
+        SplitContainerBawah.Name = "SplitContainerBawah"
+        SplitContainerBawah.Orientation = Orientation.Horizontal
+        ' 
+        ' SplitContainerBawah.Panel1
+        ' 
+        SplitContainerBawah.Panel1.Controls.Add(lvImage)
+        ' 
+        ' SplitContainerBawah.Panel2
+        ' 
+        SplitContainerBawah.Panel2.Controls.Add(SplitContainerBawah2)
+        SplitContainerBawah.Size = New Size(606, 426)
+        SplitContainerBawah.SplitterDistance = 274
+        SplitContainerBawah.TabIndex = 0
+        ' 
+        ' SplitContainerBawah2
+        ' 
+        SplitContainerBawah2.Dock = DockStyle.Fill
+        SplitContainerBawah2.Location = New Point(0, 0)
+        SplitContainerBawah2.Name = "SplitContainerBawah2"
+        ' 
+        ' SplitContainerBawah2.Panel1
+        ' 
+        SplitContainerBawah2.Panel1.Controls.Add(tcProperties)
+        ' 
+        ' SplitContainerBawah2.Panel2
+        ' 
+        SplitContainerBawah2.Panel2.Controls.Add(PictureBox1)
+        SplitContainerBawah2.Size = New Size(606, 148)
+        SplitContainerBawah2.SplitterDistance = 202
+        SplitContainerBawah2.TabIndex = 0
+        ' 
+        ' tcProperties
+        ' 
+        tcProperties.Controls.Add(TabPage1)
+        tcProperties.Controls.Add(TabPage2)
+        tcProperties.Dock = DockStyle.Fill
+        tcProperties.Location = New Point(0, 0)
+        tcProperties.Name = "tcProperties"
+        tcProperties.SelectedIndex = 0
+        tcProperties.Size = New Size(202, 148)
+        tcProperties.TabIndex = 0
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(PropertyGrid1)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(194, 120)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Image Properties"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' PropertyGrid1
+        ' 
+        PropertyGrid1.Dock = DockStyle.Fill
+        PropertyGrid1.Location = New Point(3, 3)
+        PropertyGrid1.Name = "PropertyGrid1"
+        PropertyGrid1.Size = New Size(188, 114)
+        PropertyGrid1.TabIndex = 0
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(lvExif)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(194, 120)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "EXIF Metadata"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' lvExif
+        ' 
+        lvExif.Columns.AddRange(New ColumnHeader() {chProperties, chValue})
+        lvExif.GridLines = True
+        lvExif.Location = New Point(3, 3)
+        lvExif.Name = "lvExif"
+        lvExif.Size = New Size(188, 111)
+        lvExif.TabIndex = 0
+        lvExif.UseCompatibleStateImageBehavior = False
+        lvExif.View = View.Details
+        ' 
+        ' chProperties
+        ' 
+        chProperties.Text = "Properties"
+        chProperties.Width = 100
+        ' 
+        ' chValue
+        ' 
+        chValue.Text = "Value"
+        chValue.Width = 85
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(SplitContainerUtama)
+        Controls.Add(StatusStrip1)
+        Name = "Form1"
+        Text = "Form1"
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainerUtama.Panel1.ResumeLayout(False)
+        SplitContainerUtama.Panel2.ResumeLayout(False)
+        CType(SplitContainerUtama, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainerUtama.ResumeLayout(False)
+        SplitContainerBawah.Panel1.ResumeLayout(False)
+        SplitContainerBawah.Panel2.ResumeLayout(False)
+        CType(SplitContainerBawah, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainerBawah.ResumeLayout(False)
+        SplitContainerBawah2.Panel1.ResumeLayout(False)
+        SplitContainerBawah2.Panel2.ResumeLayout(False)
+        CType(SplitContainerBawah2, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainerBawah2.ResumeLayout(False)
+        tcProperties.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage2.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusReady As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusDimension As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusBits As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusResolution As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusRawFormat As ToolStripStatusLabel
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents SplitContainerUtama As SplitContainer
+    Friend WithEvents tvDrive As TreeView
+    Friend WithEvents SplitContainerBawah As SplitContainer
+    Friend WithEvents SplitContainerBawah2 As SplitContainer
+    Friend WithEvents lvImage As ListView
+    Friend WithEvents chName As ColumnHeader
+    Friend WithEvents chExtension As ColumnHeader
+    Friend WithEvents chFileSize As ColumnHeader
+    Friend WithEvents chLastModified As ColumnHeader
+    Friend WithEvents chLastAccessed As ColumnHeader
+    Friend WithEvents chCreatedDate As ColumnHeader
+    Friend WithEvents chAttributes As ColumnHeader
+    Friend WithEvents tcProperties As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents PropertyGrid1 As PropertyGrid
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents lvExif As ListView
+    Friend WithEvents chProperties As ColumnHeader
+    Friend WithEvents chValue As ColumnHeader
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ #Folder Pertemuan 4/notepad/notepadKu/notepadKu/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        FormatToolStripMenuItem = New ToolStripMenuItem()
+        FormatToolStripMenuItem1 = New ToolStripMenuItem()
+        ViewToolStripMenuItem = New ToolStripMenuItem()
+        AboutToolStripMenuItem = New ToolStripMenuItem()
+        StatusStrip1 = New StatusStrip()
+        ToolStripStatusReady = New ToolStripStatusLabel()
+        ToolStripStatusLine = New ToolStripStatusLabel()
+        InsertStatusStrip = New ToolStripStatusLabel()
+        CapsStripStatus = New ToolStripStatusLabel()
+        NumStripStatus = New ToolStripStatusLabel()
+        OpenFileDialog1 = New OpenFileDialog()
+        FontDialog1 = New FontDialog()
+        PrintDialog1 = New PrintDialog()
+        PageSetupDialog1 = New PageSetupDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        PrintDocument1 = New Printing.PrintDocument()
+        TextBox1 = New TextBox()
+        MenuStrip1.SuspendLayout()
+        StatusStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, FormatToolStripMenuItem, FormatToolStripMenuItem1, ViewToolStripMenuItem, AboutToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' FormatToolStripMenuItem
+        ' 
+        FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        FormatToolStripMenuItem.Size = New Size(39, 20)
+        FormatToolStripMenuItem.Text = "Edit"
+        ' 
+        ' FormatToolStripMenuItem1
+        ' 
+        FormatToolStripMenuItem1.Name = "FormatToolStripMenuItem1"
+        FormatToolStripMenuItem1.Size = New Size(57, 20)
+        FormatToolStripMenuItem1.Text = "Format"
+        ' 
+        ' ViewToolStripMenuItem
+        ' 
+        ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        ViewToolStripMenuItem.Size = New Size(44, 20)
+        ViewToolStripMenuItem.Text = "View"
+        ' 
+        ' AboutToolStripMenuItem
+        ' 
+        AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        AboutToolStripMenuItem.Size = New Size(52, 20)
+        AboutToolStripMenuItem.Text = "About"
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusReady, ToolStripStatusLine, InsertStatusStrip, CapsStripStatus, NumStripStatus})
+        StatusStrip1.Location = New Point(0, 426)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(800, 24)
+        StatusStrip1.TabIndex = 0
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusReady
+        ' 
+        ToolStripStatusReady.Name = "ToolStripStatusReady"
+        ToolStripStatusReady.Size = New Size(42, 19)
+        ToolStripStatusReady.Text = "Ready."
+        ' 
+        ' ToolStripStatusLine
+        ' 
+        ToolStripStatusLine.BorderSides = ToolStripStatusLabelBorderSides.Left Or ToolStripStatusLabelBorderSides.Right
+        ToolStripStatusLine.BorderStyle = Border3DStyle.Etched
+        ToolStripStatusLine.Name = "ToolStripStatusLine"
+        ToolStripStatusLine.Size = New Size(39, 19)
+        ToolStripStatusLine.Text = "Line1"
+        ' 
+        ' InsertStatusStrip
+        ' 
+        InsertStatusStrip.BorderSides = ToolStripStatusLabelBorderSides.Right
+        InsertStatusStrip.BorderStyle = Border3DStyle.Etched
+        InsertStatusStrip.Enabled = False
+        InsertStatusStrip.Name = "InsertStatusStrip"
+        InsertStatusStrip.Size = New Size(29, 19)
+        InsertStatusStrip.Text = "INS"
+        ' 
+        ' CapsStripStatus
+        ' 
+        CapsStripStatus.BorderSides = ToolStripStatusLabelBorderSides.Right
+        CapsStripStatus.BorderStyle = Border3DStyle.Etched
+        CapsStripStatus.Enabled = False
+        CapsStripStatus.Name = "CapsStripStatus"
+        CapsStripStatus.Size = New Size(40, 19)
+        CapsStripStatus.Text = "CAPS"
+        ' 
+        ' NumStripStatus
+        ' 
+        NumStripStatus.BorderSides = ToolStripStatusLabelBorderSides.Right
+        NumStripStatus.BorderStyle = Border3DStyle.Etched
+        NumStripStatus.Enabled = False
+        NumStripStatus.Name = "NumStripStatus"
+        NumStripStatus.Size = New Size(39, 19)
+        NumStripStatus.Text = "NUM"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.DefaultExt = "*.txt"
+        OpenFileDialog1.FileName = "Untitled"
+        OpenFileDialog1.Filter = "Text File|*.txt|All Files|*.*"
+        OpenFileDialog1.Title = "Open File Text"
+        ' 
+        ' PrintDialog1
+        ' 
+        PrintDialog1.UseEXDialog = True
+        ' 
+        ' PageSetupDialog1
+        ' 
+        PageSetupDialog1.EnableMetric = True
+        ' 
+        ' SaveFileDialog1
+        ' 
+        SaveFileDialog1.DefaultExt = "*.txt"
+        SaveFileDialog1.FileName = "Untitled.txt"
+        SaveFileDialog1.Filter = "Text File|*.txt|All Files|*.*"
+        SaveFileDialog1.Title = "Save File Text as"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.AcceptsReturn = True
+        TextBox1.AcceptsTab = True
+        TextBox1.AllowDrop = True
+        TextBox1.Dock = DockStyle.Fill
+        TextBox1.Location = New Point(0, 24)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ScrollBars = ScrollBars.Both
+        TextBox1.Size = New Size(800, 402)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(StatusStrip1)
+        Controls.Add(MenuStrip1)
+        KeyPreview = True
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "NotepadKu"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusReady As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLine As ToolStripStatusLabel
+    Friend WithEvents InsertStatusStrip As ToolStripStatusLabel
+    Friend WithEvents CapsStripStatus As ToolStripStatusLabel
+    Friend WithEvents NumStripStatus As ToolStripStatusLabel
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PageSetupDialog1 As PageSetupDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
